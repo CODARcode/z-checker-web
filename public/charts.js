5bb11dde5eb18ce616901c40075c42c1718d4dc1
Hanqi Guo <hguo@anl.gov>
2016-12-19 12:41:54 -0600

    highcharts

--- public/charts.js
+++ public/charts.js
@@ -6,135 +6,77 @@ function updateProperties(data) {
   }
 }
 
-function chart(name, json) {
-  $("#charts").append("<div class='chart' id='" + name + "'></div>");
-  $("#" + name).append(name);
-}
-
-
-function updateFFTAmp(data) {
-  const W = 800, H = 150;
-  const margin = {top: 10, right: 20, bottom: 25, left: 50},
-        width = W - margin.left - margin.right,
-        height = H - margin.top - margin.bottom;
-
-  $("#fftAmpChart").html("");
-  var svg = d3.select("#fftAmpChart")
-    .append("svg")
-    .attr("class", "svg")
-    .attr("width", W)
-    .attr("height", H)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  var x = d3.scaleLinear()
-    .rangeRound([0, width])
-    .domain(d3.extent(data, function(d) {return d[0];}));
-  var yLog = d3.scaleLog()
-    .clamp(true)
-    .rangeRound([height, 0])
-    .nice()
-    .domain([0.000001, d3.max(data, function(d) {return d[1];})]);
-  var yLinear = d3.scaleLinear()
-    .rangeRound([height, 0])
-    .domain(d3.extent(data, function(d) {return d[1];}));
-
-  var useLogScale = true;
-  var y = useLogScale ? yLog : yLinear;
-
-  var xAxis = d3.axisBottom().scale(x);
-  var yAxis = d3.axisLeft().scale(y);
-
-  svg.append("g")
-    .attr("class", "axis-x")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
-  
-  svg.append("g")
-    .attr("class", "axis-y")
-    .call(yAxis)
-    .append("text")
-    .attr("x", 60)
-    .attr("dy", "0.71em")
-    .style("text-anchor", "begin")
-    .text("fftAmp");
-
-  svg.selectAll(".bar")
-    .data(data)
-    .enter()
-    .append("rect")
-    .style("fill", "steelblue")
-    .attr("class", "bar")
-    .attr("x", function(d) {return x(d[0]);})
-    .attr("y", function(d) {return y(d[1]);})
-    .attr("width", 4)
-    .attr("height", function(d) {return height - y(d[1]);});
-
-  svg.on("click", function() {
-    toggleLogScale();
-  });
- 
-  function toggleLogScale() {
-    var y = useLogScale ? yLog : yLinear;
-    svg.selectAll(".bar")
-      .data(data)
-      .transition().delay(100).duration(100)
-      .attr("y", function(d) {return y(d[1]);})
-      .attr("height", function(d) {return height - y(d[1]);});
-    svg.select(".axis-y")
-      .transition().delay(100).duration(100)
-      .call(yAxis.scale(y));
-    useLogScale = !useLogScale;
+function updateComparison(data) {
+  $("#comparison").html("<table class='table table-striped table-bordered table-hover table-condensed' id='dataPropertyTable'><thead><tr><th>Property</th><th>Value</th></tr></thead><tbody></tbody></table>");
+  var table = $("#comparison tbody");
+  for (var key in data) {
+    table.append("<tr><td>" + key + "</td><td>" + data[key] + "</td></tr>");
   }
 }
 
+function updateFFTAmp(data) {
+  data.forEach(function(d) {
+    if (d[1] < 0.0001) d[1] = 0.0001;
+  });
+
+  var json = {
+    title: {text: "FFT Amplitude"},
+    xAxis: {
+      tickInterval: 1
+    },
+    yAxis: {
+      title: { text: "amplitude" },
+      min: 0.0001, // avoid zero
+      type: "logarithmic",
+    },
+    series: [{
+      name: "FFTAmp",
+      data: data
+    }],
+    credits: {enabled: false}
+  }
+  
+  $("#fftAmpChart").highcharts(json);
+}
+
 function updateAutoCorr(data) {
-  const W = 800, H = 150;
-  const margin = {top: 10, right: 20, bottom: 25, left: 50},
-        width = W - margin.left - margin.right,
-        height = H - margin.top - margin.bottom;
-
-  $("#autoCorrChart").html("");
-  var svg = d3.select("#autoCorrChart")
-    .append("svg")
-    .attr("class", "svg")
-    .attr("width", W)
-    .attr("height", H)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  var x = d3.scaleLinear()
-    .rangeRound([0, width])
-    .domain(d3.extent(data, function(d) {return d[0];}));
-  var y = d3.scaleLinear()
-    .rangeRound([height, 0])
-    .domain(d3.extent(data, function(d) {return d[1];}));
-
-  var xAxis = d3.axisBottom().scale(x);
-  var yAxis = d3.axisLeft().scale(y);
+  var json = {
+    title: {text: "Autocorrelation"},
+    xAxis: { 
+    },
+    yAxis: {
+      title: { text: "amplitude" }
+    },
+    series: [{
+      name: "autocorr",
+      data: data
+    }],
+    credits: {enabled: false}
+  }
   
-  svg.append("g")
-    .attr("class", "axis x")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
-  
-  svg.append("g")
-    .attr("class", "axis y")
-    .call(yAxis)
-    .append("text")
-    .attr("x", 60)
-    .attr("dy", "0.71em")
-    .style("text-anchor", "begin")
-    .text("autocoor");
+  $("#autoCorrChart").highcharts(json);
+}
 
-  svg.selectAll(".bar")
-    .data(data)
-    .enter()
-    .append("rect")
-    .style("fill", "steelblue")
-    .attr("class", "bar")
-    .attr("x", function(d) {return x(d[0]);})
-    .attr("y", function(d) {return y(d[1]);})
-    .attr("width", 4)
-    .attr("height", function(d) {return height - y(d[1]);});
+function updateDis(data) {
+  var d0 = [], d1 = [];
+  data.forEach(function(d) {
+    d0.push(d[0]);
+    d1.push(d[1]);
+  });
+
+  var json = {
+    title: {text: "Dis"},
+    xAxis: { 
+    },
+    yAxis: {
+      title: { text: "dis" }
+    },
+    series: [
+      {name: "d0", data: d0}, 
+      {name: "d1", data: d1}
+    ],
+    credits: {enabled: false}
+  }
+  
+  $("#disChart").highcharts(json);
 }
--- public/master.js
+++ public/master.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
 // $("#requestProperties").on("click", function() {
   d3.json("/ec", function(d) {
-    console.log(d);
     updateProperties(d.properties);
-    // var fftChart = new chart("fft");
-    // var autoCorr = new chart("autocorr");
     updateFFTAmp(d.fftAmp);
     updateAutoCorr(d.autoCorr);
+  });
+
+  d3.json("/sz", function(d) {
+    updateComparison(d.compare);
+    updateDis(d.dis);
   });
 });
 
