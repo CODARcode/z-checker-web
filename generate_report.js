b9491eaf33fd995280a5715a638ba25b7f6d36c9
Hanqi Guo <hguo@anl.gov>
2017-06-07 17:15:23 -0500

    tmp

--- generate_report.js
+++ generate_report.js
@@ -4,6 +4,9 @@ const path = require("path");
 const glob = require("glob");
 const execFileSync = require('child_process').execFileSync;
 
+// var cheerio = require('cheerio');
+// var $ = cheerio.load(fs.readFileSync('./template/index.html')); // TODO
+
 const mode = "png"; 
 
 const rpath_dp = "/dataProperties";
@@ -20,34 +23,106 @@ const dest_path = process.argv[2] + "/report/web";
 if (!fs.existsSync(dest_path))
   fs.mkdirSync(dest_path);
 
-var svgFileMap = {}; // key: filename, value: full path
+// generateDataPropertiesTab();
+generateCompressionResultsTab();
 
-console.log("generating svg files for dataProperties...");
-var files_dp = glob.sync(input_path_dp + "/*.p");
-files_dp.forEach(function(f) {
-  var result = generateSvgForZChecker(f, input_path_dp, rpath_dp);
-  svgFileMap[result.key] = result.svg;
-});
+function generateDataPropertiesTab() {
+  console.log("generating image files for dataProperties...");
 
-console.log("generating svg files for compressionResults...");
-var files_cp = glob.sync(input_path_cp + "/*.p");
-files_cp.forEach(function(f) {
-  var result = generateSvgForZChecker(f, input_path_cp, rpath_cp);
-  svgFileMap[result.key] = result.svg;
-});
+  var outputs = [];
+  var files_dp = glob.sync(input_path_dp + "/*.p");
+  files_dp.forEach(function(f) {
+    var result = generateFigureForZChecker(f, input_path_dp, rpath_dp);
+    const key = result.key;
+    var varname = "";
+    var property = "";
 
-console.log("generating svg files for compareCompressors...");
+    if (key.endsWith("-autocorr")) {
+      varname = key.substring(0, key.lastIndexOf("-autocorr"));
+      property = "autocorr";
+    } else if (key.endsWith("-fft-amp")) {
+      varname = key.substring(0, key.lastIndexOf("-fft-amp"));
+      property = "fft-amp";
+    } else {
+      assert.fail("unknown property");
+    }
+
+    outputs.push({
+      key: key, 
+      filename: result.filename,
+      varname: varname,
+      property: property
+    });
+  });
+
+  fs.writeFileSync(dest_path + "/dataProperties.json", JSON.stringify(outputs));
+}
+
+function generateCompressionResultsTab() {
+  console.log("generating files for compressionResults...");
+
+  var outputs = [];
+
+  var files_cp = glob.sync(input_path_cp + "/*.p");
+  files_cp.forEach(function(f) {
+    var result = generateFigureForZChecker(f, input_path_cp, rpath_cp);
+    const key = result.key;
+  
+    // nasty code
+    var strs = key.split(":");
+
+    var regExp = /\(([^)]+)\)/;
+    var regResult = regExp.exec(strs[0]);
+    var bound = regResult[1];
+
+    var compressor = strs[0].slice(0, regResult.index);
+    
+    var varname = "";
+    var property = "";
+
+    const key1 = strs[1];
+
+    if (key1.endsWith("-autocorr")) {
+      varname = key1.substring(0, key1.lastIndexOf("-autocorr"));
+      property = "autocorr";
+    } else if (key1.endsWith("-dis")) {
+      varname = key1.substring(0, key1.lastIndexOf("-dis"));
+      property = "dis";
+    } else if (key1.endsWith("-fft-amp")) {
+      varname = key1.substring(0, key1.lastIndexOf("-fft-amp"));
+      property = "fft-amp";
+    } else {
+      assert.fail("unknown property");
+    }
+    // end nasty code
+
+    outputs.push({
+      key: key.replace(/\(|\)\:/g, "_"),
+      filename: result.filename, 
+      compressor: compressor, 
+      bound: bound,
+      varname: varname,
+      property: property
+    });
+  });
+
+  fs.writeFileSync(dest_path + "/compressionResults.json", JSON.stringify(outputs));
+}
+
+/*
+console.log("generating files for compareCompressors...");
 var files_cc = glob.sync(input_path_cc + "/*.p");
 files_cc.forEach(function(f) {
-  var result = generateSvgForZChecker(f, input_path_cc_data, rpath_cc);
-  svgFileMap[result.key] = result.svg;
+  var result = generateFigureForZChecker(f, input_path_cc_data, rpath_cc);
+  // svgFileMap[result.key] = result.svg;
 });
+*/
 
-function generateSvgForZChecker(filename, cwd, rpath) { // filename: full path of .p file; cwd: working directory
+function generateFigureForZChecker(filename, cwd, rpath) { // filename: full path of .p file; cwd: working directory
   var buf = fs.readFileSync(filename).toString();
   var lines = buf.split("\n");
   var script = "";
-  var key = path.basename(filename).slice(0, -2);
+  var key = path.basename(filename).slice(0, -2).replace(/\./g, '_');
 
   lines.forEach(function(str) {
     if (str.startsWith("#") || str.startsWith("set size"))
@@ -66,36 +141,18 @@ function generateSvgForZChecker(filename, cwd, rpath) { // filename: full path o
 
   execFileSync("gnuplot", [], // arguments
       {cwd: cwd, stdio: ["pipe", 1, 2], input: script});
+      // {cwd: cwd, stdio: ["pipe", 1, "null"], input: script});
   
   const dest = dest_path + "/" + key + "." + mode;
   fs.copySync(path.join(cwd, key+"." + mode), dest);
 
   return {
-    key: key, 
-    // svg: path.join(cwd, key + ".svg")
-    // svg: path.join(rpath, key + ".svg")
-    svg: key + "." + mode
+    key, 
+    filename: key + "." + mode
   };
 }
 
-///////////////
-// console.log(svgFileMap);
 
-
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const { window } = new JSDOM("<!DOCTYPE html>");
-const $ = require('jQuery')(window);
-
-// $('<h1>Hello</h1>').appendTo('body');
-// console.log($('h1').text());
-
-$("body").append('<div class="base"></div>');
-for (key in svgFileMap) {
-  $(".base").append('<div id="' + key + '"></div>')
-    .append("<p>" + key + "</p>")
-    .append('<img src="' + svgFileMap[key] + '"/>')
-}
-
-// console.log($(":root").html());
-fs.writeFileSync(dest_path + "/index.html", $(":root").html());
+fs.copySync("./template/index.html", dest_path + "/index.html");
+fs.copySync("./template/jquery-3.1.1.min.js", dest_path + "/jquery-3.1.1.min.js");
+fs.copySync("./template/crossfilter.js", dest_path + "/crossfilter.js");
